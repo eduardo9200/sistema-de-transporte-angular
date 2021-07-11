41fed269d0ea36f531d0b057682eebf979354306
eduardo9200 <edumaia92@gmail.com>
2021-07-11 02:41:16 -0300

    Ajuste no módulo de tabela de resultados.

--- src/app/tabela-resultado/tabela-resultado.module.ts
+++ src/app/tabela-resultado/tabela-resultado.module.ts
@@ -2,19 +2,22 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TabelaComponent } from './components/tabela/tabela.component';
 import { SharedModule } from '../shared/shared.module';
+import { TabelaLinhasComponent } from './components/tabela-linhas/tabela-linhas.component';
 
 
 
 @NgModule({
   declarations: [
-    TabelaComponent
+    TabelaComponent,
+    TabelaLinhasComponent
   ],
   imports: [
     CommonModule,
     SharedModule
   ],
   exports: [
-    TabelaComponent
+    TabelaComponent,
+    TabelaLinhasComponent
   ]
 })
 export class TabelaResultadoModule { }
