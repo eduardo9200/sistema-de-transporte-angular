d7397eddbd19d3afafffd30f83aba748ffbfb210
eduardo9200 <edumaia92@gmail.com>
2021-07-11 02:35:38 -0300

    Adição de conteúdo ao componente de pesquisa de linhas.

--- src/app/pesquisa/components/pesquisa-linhas/pesquisa-linhas.component.ts
+++ src/app/pesquisa/components/pesquisa-linhas/pesquisa-linhas.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { DadosBuscaLinha, SearchItemLinha, TipoItemBuscaLinha } from '../../models/pesquisa.model';
+import { PesquisaBuilder } from '../../services/pesquisa.builder';
 
 @Component({
   selector: 'app-pesquisa-linhas',
@@ -7,8 +10,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PesquisaLinhasComponent implements OnInit {
 
-  constructor() { }
+  @Output() dadosBusca = new EventEmitter<DadosBuscaLinha>();
 
-  ngOnInit() {}
+  constructor(
+    private pesquisaBuilder: PesquisaBuilder,
+    private formBuilder: FormBuilder
+  ) { }
 
+  ngOnInit() {
+    this.searchItemList = this.pesquisaBuilder.getSearchOptionsLinha();
+    this.criaFormulario();
+  }
+
+  searchItemList: SearchItemLinha[] = [];
+  itemSelecionado: SearchItemLinha;
+  valorPesquisa: string = '';
+  inputType: string = "text";
+
+  formPesquisa: FormGroup;
+  
+  private criaFormulario() {
+    this.formPesquisa = this.formBuilder.group({
+      textoPesquisa: [null]
+    });
+  }
+
+  get textoPesquisa(): FormControl {
+    return this.formPesquisa.get('textoPesquisa') as FormControl;
+  }
+
+  public selecionouPesquisa(event) {
+    this.itemSelecionado = event.detail.value;
+    
+    if(this.itemSelecionado.id === TipoItemBuscaLinha.NUMERO) {
+      this.inputType = "number";
+    } else {
+      this.inputType = "text";
+    }
+  }
+
+  public pesquisar() {
+    this.dadosBusca.emit({
+      itemSelecionado: this.itemSelecionado,
+      textoBusca: this.textoPesquisa.value
+    });
+  }
 }
