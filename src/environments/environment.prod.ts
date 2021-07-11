952813092cf2595caa4bdf3090d3bce7210159ec
eduardo9200 <edumaia92@gmail.com>
2021-07-11 02:42:01 -0300

    Adição de variáveis no environment.prod.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,9 @@
+declare var require: any;
+
+const BASE_URL = `http://localhost:8080`;
+
 export const environment = {
-  production: true
+  production: true,
+  urlSistemaTransporteApi: BASE_URL,
+  VERSION: require('../../package.json').version
 };
